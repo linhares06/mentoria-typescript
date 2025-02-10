efc6625b4c72aa4ee32b2232baf932a2334eb205
unknown <felinhares06@gmail.com>
2025-02-10 14:42:10 -0300

    Commit do desafio de typescript

---
+++ src/desafios/desafio1.ts
@@ -0,0 +1,6 @@
+
+
+let employee: { code: number; name: string } = {
+    code: 10,
+    name: "John"
+}
---
+++ src/desafios/desafio2.ts
@@ -0,0 +1,32 @@
+
+
+type Person = {
+    name: string;
+    age: number;
+    profession: string;
+};
+
+let pessoa1: Person = {
+    name: "maria",
+    age: 29,
+    profession: "atriz"
+
+};
+
+let pessoa2: Person = {
+    name: "roberto",
+    age: 19,
+    profession: "Padeiro"
+};
+
+let pessoa3: Person = {
+    name: "laura",
+    age: 32,
+    profession: "Atriz"
+};
+
+let pessoa4: Person = {
+    name: "carlos",
+    age: 19,
+    profession: "padeiro"
+}
